b4f271bc6543ab0f75465b1fb206550bfbe62935
Arturs Bricis <bricis.artur@gmail.com>
2021-10-31 21:10:43 +0200

    season service implementation

--- internal/service/seasonService.go
+++ internal/service/seasonService.go
@@ -1,7 +1,10 @@
 package service
 
 import (
+	"context"
 	"transmission-web-scrapper/internal/db"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type SeasonService struct {
@@ -12,4 +15,44 @@ func NewSeasonService(repo db.SeasonRepository) SeasonService {
 	return SeasonService{
 		repo: repo,
 	}
+}
+
+func (s SeasonService) CreateSeason(ctx context.Context, season db.Season) (db.Season, error) {
+	result, err := s.repo.Create(ctx, season)
+	if err != nil {
+		return db.Season{}, err
+	}
+	return result, nil
+}
+
+func (s SeasonService) UpdateSeason(ctx context.Context, season db.Season) (db.Season, error) {
+	result, err := s.repo.Update(ctx, season)
+	if err != nil {
+		return db.Season{}, err
+	}
+	return result, nil
+}
+
+func (s SeasonService) DeleteSeason(ctx context.Context, id primitive.ObjectID) error {
+	err := s.repo.Delete(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s SeasonService) GetSeason(ctx context.Context, id primitive.ObjectID) (db.Season, error) {
+	result, err := s.repo.Get(ctx, id)
+	if err != nil {
+		return db.Season{}, err
+	}
+	return result, nil
+}
+
+func (s SeasonService) GetAllSeason(ctx context.Context) ([]db.Season, error) {
+	result, err := s.repo.GetAll(ctx)
+	if err != nil {
+		return []db.Season{}, err
+	}
+	return result, nil
 }
