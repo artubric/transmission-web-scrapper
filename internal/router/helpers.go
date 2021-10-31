c3ed4975f167a2b7de2959e654db4673c000d6a8
Arturs Bricis <bricis.artur@gmail.com>
2021-10-31 21:11:15 +0200

    new helpers for routes

--- internal/router/helpers.go
+++ internal/router/helpers.go
@@ -5,7 +5,10 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"transmission-web-scrapper/internal/db"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (rt Router) writeErrorJSON(w http.ResponseWriter, err error) {
@@ -19,12 +22,20 @@ func (rt Router) writeErrorJSON(w http.ResponseWriter, err error) {
 func (rt Router) unmarshallDataSource(body []byte) (db.DataSource, error) {
 	var dataSource db.DataSource
 	if err := json.Unmarshal(body, &dataSource); err != nil {
-		return dataSource, err
+		return db.DataSource{}, err
 	}
 	return dataSource, nil
 }
 
-func (rt Router) unmarshalBody(w http.ResponseWriter, r *http.Request) (db.DataSource, error) {
+func (rt Router) unmarshallSeason(body []byte) (db.Season, error) {
+	var season db.Season
+	if err := json.Unmarshal(body, &season); err != nil {
+		return db.Season{}, err
+	}
+	return season, nil
+}
+
+func (rt Router) unmarshalBodyToDataSource(w http.ResponseWriter, r *http.Request) (db.DataSource, error) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -42,6 +53,24 @@ func (rt Router) unmarshalBody(w http.ResponseWriter, r *http.Request) (db.DataS
 	return ds, nil
 }
 
+func (rt Router) unmarshalBodyToSeason(w http.ResponseWriter, r *http.Request) (db.Season, error) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		rt.writeErrorJSON(w, err)
+		log.Printf("Cannot parse the body: %+v\n", err)
+		return db.Season{}, err
+	}
+	ds, err := rt.unmarshallSeason(body)
+	if err != nil {
+		log.Printf("Cannot unmarshal the body: %+v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		rt.writeErrorJSON(w, err)
+		return db.Season{}, err
+	}
+	return ds, nil
+}
+
 func (rt Router) marshalObject(i interface{}) ([]byte, error) {
 	resultJSON, err := json.Marshal(&i)
 	if err != nil {
@@ -52,6 +81,7 @@ func (rt Router) marshalObject(i interface{}) ([]byte, error) {
 
 func (rt Router) handleResult(i interface{}, err error, w http.ResponseWriter) {
 	if err != nil {
+		log.Printf("Failed with: %+v\n", err)
 		rt.writeErrorJSON(w, err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -64,4 +94,10 @@ func (rt Router) handleResult(i interface{}, err error, w http.ResponseWriter) {
 	}
 	w.Write(jsonResponse)
 	w.WriteHeader(http.StatusOK)
+}
+
+func (rt Router) getPathParam(path string) (primitive.ObjectID, error) {
+	pathSlice := strings.Split(path, "/")
+	inputId := pathSlice[len(pathSlice)-1]
+	return primitive.ObjectIDFromHex(inputId)
 }
